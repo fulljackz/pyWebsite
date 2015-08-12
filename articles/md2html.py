77ba7fb558503577d5c604bd594efab899d92d34
Konosprod <konosprod@free.fr>
2015-08-12 08:54:06 +0200

    Update md2html.py
    Created function, removed a useless loop and add a bit of error handling

--- articles/md2html.py
+++ articles/md2html.py
@@ -7,34 +7,51 @@ import datetime
 import os
 import fileinput
 
-# Open arg file and parse lines by lines
-article = open(sys.argv[1], mode="r",encoding="utf-8")
-lines = article.readlines()  
+def printHelp():
+	print("Usage : ./md2html.py article.md") 
 
-# Set article content from line 7 to eof
-for line in lines:
+def main():
+
+	if len(sys.argv) != 2:
+		printHelp()
+		return
+
+	# Open arg file and parse lines by lines
+	try:
+		article = open(sys.argv[1], mode="r",encoding="utf-8")
+	except IOError:
+		print(sys.argv[1] + " doesn't exists.")
+		return
+		
+	lines = article.readlines()  
+
+	# Set article content from line 7 to eof
 	articleContent = lines[6:]
 
-# Set metadata to list
-articleMetaData = []
-i = 0
-while i < 4:
-	articleMetaData.append(lines[i].strip().split(':', 1)[1])
-	i += 1
+	# Set metadata to list
+	articleMetaData = []
+	i = 0
+	while i < 4:
+		articleMetaData.append(lines[i].strip().split(':', 1)[1])
+		i += 1
 
-# Replace tags in template with metadata
-template = open("template.html", "r").read()
-template = template.replace("#DATE#", str(datetime.date.today()))
-template = template.replace("#AUTHOR#", articleMetaData[0])
-template = template.replace("#TITLEPOST#", articleMetaData[1])
-template = template.replace("#TAGS#", articleMetaData[2])
-template = template.replace("#CATEGORY#", articleMetaData[3])
-template = template.replace("#CONTENT#", markdown.markdown(''.join(articleContent), output_format="html5"))
-template = template.replace("#YEAR#", str(datetime.datetime.today().year))
+	# Replace tags in template with metadata
+	template = open("template.html", "r").read()
+	template = template.replace("#DATE#", str(datetime.date.today()))
+	template = template.replace("#AUTHOR#", articleMetaData[0])
+	template = template.replace("#TITLEPOST#", articleMetaData[1])
+	template = template.replace("#TAGS#", articleMetaData[2])
+	template = template.replace("#CATEGORY#", articleMetaData[3])
+	template = template.replace("#CONTENT#", markdown.markdown(''.join(articleContent), output_format="html5"))
+	template = template.replace("#YEAR#", str(datetime.datetime.today().year))
 
-# Create html file with metadata title + html
-output = codecs.open(articleMetaData[1].lstrip().replace(" ", "_")+".html", "w", encoding="utf-8", errors="xmlcharrefreplace")
+	# Create html file with metadata title + html
+	output = codecs.open(articleMetaData[1].lstrip().replace(" ", "_")+".html", "w", encoding="utf-8", errors="xmlcharrefreplace")
 
-# Write file
-output.write(template)
-output.close()
+	# Write file
+	output.write(template)
+	output.close()
+	
+if __name__ == "__main__":
+	main()
+	
