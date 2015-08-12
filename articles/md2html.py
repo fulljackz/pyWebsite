98cb55767005419d5c56b089dcef870a9cee4653
Manu <manu@lacavernedemanu.fr>
2015-08-12 21:03:18 +0200

    Add build index.html function

--- articles/md2html.py
+++ articles/md2html.py
@@ -6,35 +6,35 @@ import codecs
 import datetime
 import os
 import fileinput
+import re
+
+workingDir = "/home/manu/Documents/Devlopments/Python/pyWebsite/articles"
+now = datetime.date.today()
 
 def printHelp():
 	print("Usage : ./md2html.py article.md") 
 
 def main():
-
+	path = workingDir + "/" + str(now.year)
+	#~ os.makedirs(path, exist_ok=True)
 	if len(sys.argv) != 2:
 		printHelp()
 		return
-
 	# Open arg file and parse lines by lines
 	try:
 		article = open(sys.argv[1], mode="r",encoding="utf-8")
 	except IOError:
 		print(sys.argv[1] + " doesn't exists.")
 		return
-		
 	lines = article.readlines()  
-
 	# Set article content from line 7 to eof
 	articleContent = lines[6:]
-
 	# Set metadata to list
 	articleMetaData = []
 	i = 0
 	while i < 4:
 		articleMetaData.append(lines[i].strip().split(':', 1)[1])
 		i += 1
-
 	# Replace tags in template with metadata
 	template = open("template.html", "r").read()
 	template = template.replace("#DATE#", str(datetime.date.today()))
@@ -44,14 +44,30 @@ def main():
 	template = template.replace("#CATEGORY#", articleMetaData[3])
 	template = template.replace("#CONTENT#", markdown.markdown(''.join(articleContent), output_format="html5"))
 	template = template.replace("#YEAR#", str(datetime.datetime.today().year))
-
 	# Create html file with metadata title + html
-	output = codecs.open(articleMetaData[1].lstrip().replace(" ", "_")+".html", "w", encoding="utf-8", errors="xmlcharrefreplace")
-
+	output = codecs.open(path + "/" + articleMetaData[1].lstrip().replace(" ", "_")+".html", "w", encoding="utf-8", errors="xmlcharrefreplace")
 	# Write file
 	output.write(template)
 	output.close()
-	
+
+def buildIndex():
+	workingDir = "/home/manu/Documents/Devlopments/Python/pyWebsite/articles"
+	now = datetime.date.today()
+	# Build index with articles abstract
+	for file in os.listdir(workingDir):
+		if file.endswith(".md"):
+			infile = open(workingDir + "/" + file,mode="r").read()
+			abstracts = re.findall(r'<!---Abstract(.*?)Abstract-->',infile,re.DOTALL)
+			print(abstracts)
+			index = open("index_template.html", "r").read()
+			index = index.replace("#ARTICLE-SUMMARY#", markdown.markdown(''.join(abstracts), output_format="html5"))
+			output = codecs.open((workingDir + "/" + "index.html"), "w", encoding="utf-8", errors="xmlcharrefreplace")
+			output.write(index)
+			output.close()
+
+
 if __name__ == "__main__":
 	main()
+	buildIndex()
+
 	
